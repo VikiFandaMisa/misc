e44804167532c4a0b49eb883f907c1bf9b6f5cf5
Michal Černý <cernyymichal@gmail.com>
2020-04-13 13:42:15 +0200

    add test admin script

---
+++ sql/insert_test_admin.sql
@@ -0,0 +1,10 @@
+use db_backupservice;
+
+insert into accounts values (
+	"test",
+    "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8",
+    true,
+    "test@test.test",
+    false,
+    ""
+)
