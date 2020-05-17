781faeb4f6215e0aa064af0f2ec49e6f91d36bb2
Michal Černý <cernyymichal@gmail.com>
2020-05-17 23:48:11 +0200

    date_add

--- sql/insert_test_template.sql
+++ sql/insert_test_template.sql
@@ -1,6 +1,6 @@
 use db_BackupService;
 
-insert into Templates values (null, "testTemplate", "0 4 * * */1", 1, 1, now(), now(), false, 2);
+insert into Templates values (null, "testTemplate", "0 4 * * */1", 1, 1, now(), DATE_ADD(now(), INTERVAL 7 DAY), false, 2);
 
 insert into Paths values (null, 1, null, true, "C:\\Users\\Michal\\Desktop\\test1");
 insert into Paths values (null, 1, null, true, "C:\\Users\\Michal\\Desktop\\test2");
