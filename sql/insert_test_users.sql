962737580bbc692e9fa903b23e132a9db8bc144f
Michal Černý <cernyymichal@gmail.com>
2020-06-18 20:38:33 +0200

    remove reportperiod

--- sql/insert_test_users.sql
+++ sql/insert_test_users.sql
@@ -2,15 +2,15 @@ use db_BackupService;
 
 DELETE FROM Accounts WHERE Username = "test_admin";
 
-INSERT INTO Accounts (ID, Username, Password, Admin, Email, SendReports, ReportPeriod)
-VALUES (NULL, "test_admin", "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8", true, "cernymichal@sssvt.cz", false, "");
+INSERT INTO Accounts (ID, Username, Password, Admin, Email, SendReports)
+VALUES (NULL, "test_admin", "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8", true, "cernymichal@sssvt.cz", false);
 
 DELETE FROM Accounts WHERE Username = "test" ;
 
-INSERT INTO Accounts (ID, Username, Password, Admin, Email, SendReports, ReportPeriod)
-VALUES (NULL, "test", "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8", false, "frantisekkriz@sssvt.cz", false, "");
+INSERT INTO Accounts (ID, Username, Password, Admin, Email, SendReports)
+VALUES (NULL, "test", "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8", false, "frantisekkriz@sssvt.cz", false);
 
 DELETE FROM Accounts WHERE Username = "test_victor" ;
 
-INSERT INTO Accounts (ID, Username, Password, Admin, Email, SendReports, ReportPeriod)
-VALUES (NULL, "test_victor", "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8", true, "smithvictormaxim@sssvt.cz", true, "");
+INSERT INTO Accounts (ID, Username, Password, Admin, Email, SendReports)
+VALUES (NULL, "test_victor", "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8", true, "smithvictormaxim@sssvt.cz", true);
